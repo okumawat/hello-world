4feaf6a5a9f9bd099c23b835bbd7c8e44f2a125d
Om <omkumawat168@gmail.com>
2018-11-11 13:11:17 +0530

    Added function

--- imagep.py
+++ imagep.py
@@ -1,7 +1,11 @@
 import cv2
 import numpy as np
 imageFile = input("Enter Image file name:")
-img = cv2.imread(imageFile,0)
-cv2.imshow('flower',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+def showImage(imageFile):
+    img = cv2.imread(imageFile,0)
+    cv2.imshow('flower',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
