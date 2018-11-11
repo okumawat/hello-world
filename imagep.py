66963681cf7cfe9a250ae34d93735614084cc730
Om <omkumawat168@gmail.com>
2018-11-11 14:28:56 +0530

    added main function

--- imagep.py
+++ imagep.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
-imageFile = input("Enter Image file name:")
-
 def showImage(imageFile):
     img = cv2.imread(imageFile,0)
     cv2.imshow('flower',img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+if __name__=="__main__":
+    imageFile = input("Enter Image file name:")
+    showImage(imageFile)
 
